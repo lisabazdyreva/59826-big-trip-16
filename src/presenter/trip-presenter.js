d827f2e9b5c30fb56019a910ae1a6fbe1572b1e6
Lisa <lisabazdyreva@gmail.com>
2021-12-13 17:57:04 +0300

    Экземпляр презентера trip подключен в main и инициализирует работу предложения

--- src/presenter/trip-presenter.js
+++ src/presenter/trip-presenter.js
@@ -1,48 +1,34 @@
 import {DefaultValue, RenderPosition} from '../consts';
+import {render, replaceChild} from '../utils/render-utils';
+import {isEsc} from '../utils/utils';
 
-import MenuView from '../view/menu-view';
-import FiltersView from '../view/filters-view';
 import PointsListView from '../view/points-list-view';
 import EmptyListView from '../view/empty-list-view';
-import {render, replaceChild} from '../utils/render-utils';
 import InfoView from '../view/info-view';
 import SortingView from '../view/sorting-view';
 import PointView from '../view/point-view';
 import EditPointView from '../view/edit-point-view';
-import {isEsc} from '../utils/utils';
 
 
 export default class TripPresenter {
 
-  #menuContainer = null;
-  #filtersContainer = null;
   #mainContainer = null;
   #infoContainer = null;
 
-  #menuComponent = new MenuView(DefaultValue.MENU);
-  #filtersComponent = new FiltersView(DefaultValue.FILTER);
   #pointsListComponent = new PointsListView();
   #emptyListComponent = new EmptyListView(DefaultValue.NOTIFICATION);
 
   #tripPoints = [];
 
-  constructor(menuContainer, filtersContainer, mainContainer, infoContainer) {
-    this.#menuContainer = menuContainer;
-    this.#filtersContainer = filtersContainer;
+  constructor(mainContainer, infoContainer) {
     this.#mainContainer = mainContainer;
     this.#infoContainer = infoContainer;
   }
 
   init = (points) => {
     this.#tripPoints = [...points];
-  }
 
-  renderMenu = () => {
-    render(this.#menuContainer, this.#menuComponent, RenderPosition.BEFOREEND);
-  }
-
-  renderFilters = () => {
-    render(this.#filtersContainer, this.#filtersComponent, RenderPosition.BEFOREEND);
+    this.renderMainContent();
   }
 
   renderPoint = (container, point) => {
@@ -79,20 +65,40 @@ export default class TripPresenter {
     render(container, pointComponent, RenderPosition.BEFOREEND);
   }
 
+  renderPoints = () => {
+    this.#tripPoints.slice().forEach((point) => this.renderPoint(this.#pointsListComponent, point));
+  }
+
+  renderInfo = () => {
+    const infoComponent = new InfoView(this.#tripPoints);
+    render(this.#infoContainer, infoComponent, RenderPosition.AFTERBEGIN);
+  }
+
+  renderSorting = () => {
+    const sortingComponent = new SortingView(DefaultValue.SORTING);
+    render(this.#mainContainer, sortingComponent, RenderPosition.BEFOREEND);
+  }
+
+  renderPointsList = () => {
+    render(this.#mainContainer, this.#pointsListComponent, RenderPosition.BEFOREEND);
+    this.renderPoints();
+  }
+
+  renderTripComponents = () => {
+    this.renderInfo();
+    this.renderSorting();
+    this.renderPointsList();
+  }
+
   renderMainContent = () => {
     if (!this.#tripPoints.length) {
-      render(this.#mainContainer, this.#emptyListComponent, RenderPosition.BEFOREEND);
+      this.renderEmptyList();
     } else {
-      const infoComponent = new InfoView(this.#tripPoints);
-      const sortingComponent = new SortingView(DefaultValue.SORTING);
-
-      render(this.#infoContainer, infoComponent, RenderPosition.AFTERBEGIN);
-      render(this.#mainContainer, sortingComponent, RenderPosition.BEFOREEND);
-      render(this.#mainContainer, this.#pointsListComponent, RenderPosition.BEFOREEND);
-
-      this.#tripPoints.forEach((point) => this.renderPoint(this.#pointsListComponent, point));
-
+      this.renderTripComponents();
     }
   }
 
+  renderEmptyList = () => {
+    render(this.#mainContainer, this.#emptyListComponent, RenderPosition.BEFOREEND);
+  }
 }
